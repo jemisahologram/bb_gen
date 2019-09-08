bd278a911e4a202e08aebf5f2ffc617202ca28bd
jemisahologram <lauren.abineri@gmail.com>
2019-09-08 15:02:34 +1000

    initialising project

---
+++ bb_gen.js
@@ -0,0 +1,308 @@
+//Q, A, Z - Seal_Kiss_From_A_Rose
+
+//Q
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 81 ) {
+        document.getElementById('seal_bb2_bay').play();
+        document.getElementById("body").style.backgroundColor = "Violet";
+    }
+});
+
+document.addEventListener('keyup', function(e) {
+    if (e.keyCode == 81) {
+        document.getElementById('seal_bb2_beh').play();
+        document.getElementById("body").style.backgroundColor = "SlateBlue";
+        document.getElementById("beeehhh").innerHTML = "beeehhh";
+    }
+});
+
+
+//A
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 65) {
+        document.getElementById('seal_bb2_bay').play();
+        document.getElementById("body").style.backgroundColor = "Violet";
+        document.getElementById("bay").innerHTML = "bay";
+    }
+});
+
+//Z
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 90) {
+        document.getElementById('seal_bb2_beh').play();
+        document.getElementById("body").style.backgroundColor = "SlateBlue";
+        document.getElementById("beeehhh").innerHTML = "beeehhh";
+       
+    }
+});
+
+
+//W, S, X - Xtina_Genie_In_A_Bottle
+
+//W
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 87) {
+        document.getElementById('genie_bb_bay').play();
+        document.getElementById("body").style.backgroundColor = "Salmon";
+    }
+});
+
+document.addEventListener('keyup', function(e) {
+    if (e.keyCode == 87) {
+        document.getElementById('genie_bb_beh').play();
+        document.getElementById("body").style.backgroundColor = "Turquoise";
+    }
+});
+
+//S
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 83) {
+        document.getElementById('genie_bb_bay').play();
+        document.getElementById("body").style.backgroundColor = "Salmon";
+        document.getElementById("bay").innerHTML = "bay";
+        
+    }
+});
+
+   
+//X
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 88) {
+        document.getElementById('genie_bb_beh').play();
+        document.getElementById("body").style.backgroundColor = "Turquoise";
+        document.getElementById("beeehhh").innerHTML = "beeehhh";
+    }
+});
+
+//E, D, C - Bieber_Baby
+
+//E
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 69) {
+        document.getElementById('bieber_bb_bay').play();
+        document.getElementById("body").style.backgroundColor = "Aqua";
+        document.getElementById("bay").innerHTML = "bay";
+    }
+});
+
+document.addEventListener ('keyup', function(e) {
+    if (e.keyCode == 69) {
+        document.getElementById('bieber_bb_beh').play();
+        document.getElementById("body").style.backgroundColor = "SteelBlue";
+        document.getElementById("beeehhh").innerHTML = "beeehhh";
+    }
+});
+
+//D
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 68) {
+        document.getElementById('bieber_bb_bay').play();
+        document.getElementById("body").style.backgroundColor = "Aqua";
+        document.getElementById("bay").innerHTML = "bay";
+    }
+});
+
+//C
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 67) {
+        document.getElementById('bieber_bb_beh').play();
+        document.getElementById("body").style.backgroundColor = "SteelBlue";
+        document.getElementById("beeehhh").innerHTML = "beeehhh";
+    
+    }
+});
+
+//R, F, V - BigMountain_Baby_I_Love_Your_Way
+
+//R
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 82) {
+        document.getElementById('mount_bb_bay').play();
+        document.getElementById("body").style.backgroundColor = "PeachPuff";
+        document.getElementById("bay").innerHTML = "bay";
+    }
+});
+
+document.addEventListener ('keyup', function(e) {
+    if (e.keyCode == 82) {
+        document.getElementById('mount_bb_beh').play();
+        document.getElementById("body").style.backgroundColor = "Peru";
+        document.getElementById("beeehhh").innerHTML = "beeehhh";
+    }
+});
+
+//F
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 70) {
+        document.getElementById('mount_bb_bay').play();
+        document.getElementById("body").style.backgroundColor = "PeachPuff";
+        document.getElementById("bay").innerHTML = "bay";
+    }
+});
+
+//V
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 86) {
+        document.getElementById('mount_bb_beh').play();
+        document.getElementById("body").style.backgroundColor = "Peru";
+        document.getElementById("beeehhh").innerHTML = "beeehhh";
+    
+    }
+});
+
+//T, G, B - Warpaint_Baby
+
+//T
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 84) {
+        document.getElementById('warpaint_bb_bay').play();
+        document.getElementById("body").style.backgroundColor = "DarkOliveGreen";
+        document.getElementById("bay").innerHTML = "bay";
+    }
+});
+
+document.addEventListener ('keyup', function(e) {
+    if (e.keyCode == 84) {
+        document.getElementById('warpaint_bb_beh').play();
+        document.getElementById("body").style.backgroundColor = "PaleVioletRed";
+        document.getElementById("beeehhh").innerHTML = "beeehhh";
+    }
+});
+
+//G
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 71) {
+        document.getElementById('warpaint_bb_bay').play();
+        document.getElementById("body").style.backgroundColor = "DarkOliveGreen";
+        document.getElementById("bay").innerHTML = "bay";
+    }
+});
+
+//B
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 66) {
+        document.getElementById('warpaint_bb_beh').play();
+        document.getElementById("body").style.backgroundColor = "PaleVioletRed";
+        document.getElementById("beeehhh").innerHTML = "beeehhh";
+    
+    }
+});
+
+//Y, H, N - Seal_KissFromARose_1
+
+//Y
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 89) {
+        document.getElementById('seal_bb1_bay').play();
+        document.getElementById("body").style.backgroundColor = "DarkSlateGrey";
+        document.getElementById("bay").innerHTML = "bay";
+    }
+});
+
+document.addEventListener ('keyup', function(e) {
+    if (e.keyCode == 89) {
+        document.getElementById('seal_bb1_beh').play();
+        document.getElementById("body").style.backgroundColor = "Thistle";
+        document.getElementById("beeehhh").innerHTML = "beeehhh";
+    }
+});
+
+//H
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 72) {
+        document.getElementById('seal_bb1_bay').play();
+        document.getElementById("body").style.backgroundColor = "DarkSlateGrey";
+        document.getElementById("bay").innerHTML = "bay";
+    }
+});
+
+//N
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 78) {
+        document.getElementById('seal_bb1_beh').play();
+        document.getElementById("body").style.backgroundColor = "Thistle";
+        document.getElementById("beeehhh").innerHTML = "beeehhh";
+    
+    }
+});
+
+//U, J, M - TheRonettes_BeMyLittleBayBeh
+
+//U
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 85) {
+        document.getElementById('ronettes_bb_bay').play();
+        document.getElementById("body").style.backgroundColor = "Crimson";
+        document.getElementById("bay").innerHTML = "bay";
+    }
+});
+
+document.addEventListener ('keyup', function(e) {
+    if (e.keyCode == 85) {
+        document.getElementById('ronettes_bb_beh').play();
+        document.getElementById("body").style.backgroundColor = "Cyan";
+        document.getElementById("beeehhh").innerHTML = "beeehhh";
+    }
+});
+
+//J
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 74) {
+        document.getElementById('ronettes_bb_bay').play();
+        document.getElementById("body").style.backgroundColor = "Crimson";
+        document.getElementById("bay").innerHTML = "bay";
+    }
+});
+
+//M
+document.addEventListener ('keydown', function(e) {
+    if (e.keyCode == 77) {
+        document.getElementById('ronettes_bb_beh').play();
+        document.getElementById("body").style.backgroundColor = "Cyan";
+        document.getElementById("beeehhh").innerHTML = "beeehhh";
+    }
+});
+
+//I, K, (,) -  Brit_BBOneMoreTime 
+
+//I
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 73) {
+        document.getElementById('brit_bb_bay').play();
+        document.getElementById("body").style.backgroundColor = "Red";
+        document.getElementById("bay").innerHTML = "bay";
+    }
+});
+
+document.addEventListener ('keyup', function(e) {
+    if (e.keyCode == 73) {
+        document.getElementById('brit_bb_beh').play();
+        document.getElementById("body").style.backgroundColor = "Pink";
+        document.getElementById("beeehhh").innerHTML = "beeehhh";
+    }
+});
+
+//K
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 75) {
+        document.getElementById('brit_bb_bay').play();
+        document.getElementById("body").style.backgroundColor = "Red";
+        document.getElementById("bay").innerHTML = "bay";
+    }
+});
+
+//(,)
+document.addEventListener ('keydown', function(e) {
+    if (e.keyCode == 188) {
+        document.getElementById('brit_bb_beh').play();
+        document.getElementById("body").style.backgroundColor = "Pink";
+        document.getElementById("beeehhh").innerHTML = "beeehhh";
+    }
+});
+
+
+
+
+
+
+
